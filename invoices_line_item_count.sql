903fd99b4fb8e76db71b2ec5384d54d90fc9c75e
Talbot <talbotlawrence75@gmail.com>
2017-04-28 12:13:10 -0500

    included all of the invoice table columns

--- invoices_line_item_count.sql
+++ invoices_line_item_count.sql
@@ -1,3 +1,4 @@
 --Provide a query that shows all Invoices but includes the # of invoice line items.
-select inl.invoicelineid, inv.invoicedate, inv.[total]
-from Invoice inv join InvoiceLine inl on inv.invoiceid = inl.invoiceid; 
+select inl.invoicelineid, inv.*
+from Invoice inv 
+join InvoiceLine inl on inv.invoiceid = inl.invoiceid; 
