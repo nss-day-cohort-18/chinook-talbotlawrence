6632cf35f1aaf3b3191203b5e0b3cb1ed03e40fd
Talbot <talbotlawrence75@gmail.com>
2017-04-28 11:47:51 -0500

    I got top_2013_track.sql to only display the top 9 tracks dynamically, but it is super ugly

--- top_2013_track.sql
+++ top_2013_track.sql
@@ -19,6 +19,7 @@ order by sum(tra.unitprice) desc
 limit 1;
 
 --number of units sold
+/*
 select tra.name	, count(inv.invoiceid) as number_of_tracks_sold
 from Track tra 
 join InvoiceLine inl on tra.trackid = inl.trackid
@@ -27,4 +28,24 @@ where substr(inv.invoicedate, 0, 5) = '2013'
 group by tra.name
 having count(inv.invoiceid) > (select count(*) from Track where name = 'Looking For Love')   --this still sucks
 order by count(inv.invoiceid) desc;
+*/
 ---------------------------------------------------------------------------
+--This is so freakin ugly, BUT IT WORKS!!
+select tra.name	, count(inv.invoiceid) as number_of_tracks_sold
+from Track tra 
+join InvoiceLine inl on tra.trackid = inl.trackid
+join Invoice inv on inl.invoiceid = inv.invoiceid
+where substr(inv.invoicedate, 0, 5) = '2013'
+group by tra.name
+having count(inv.invoiceid) = 
+	(
+	select count(inv.invoiceid)
+	from Track tra 
+	join InvoiceLine inl on tra.trackid = inl.trackid
+	join Invoice inv on inl.invoiceid = inv.invoiceid
+	where substr(inv.invoicedate, 0, 5) = '2013'
+	group by tra.name
+	order by count(inv.invoiceid) desc
+	limit 1
+	)   
+order by count(inv.invoiceid) desc;
