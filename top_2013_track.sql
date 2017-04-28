9f2596b20e919ef0c5a76bf9961707ac9502f6e7
Talbot <talbotlawrence75@gmail.com>
2017-04-28 11:56:38 -0500

    Cleaned up the top_2013_track.sql

--- top_2013_track.sql
+++ top_2013_track.sql
@@ -1,35 +1,4 @@
 --Provide a query that shows the most purchased track of 2013.
---
-/*
-select  tra.name, inv.invoicedate, "$" || printf("%.2f", sum(inv.[total])) as total_price
-from Track tra join InvoiceLine inl on tra.trackid = inl.trackid
-	join Invoice inv on inl.invoiceid = inv.invoiceid
-where substr(inv.invoicedate, 0, 5) = '2013'
-group by tra.name
-order by sum(inv.[total]) desc
-limit 1;
-*/
---price
-select  tra.name, inv.invoicedate, "$" || printf("%.2f", sum(tra.unitprice)) as total_price
-from Track tra join InvoiceLine inl on tra.trackid = inl.trackid
-	join Invoice inv on inl.invoiceid = inv.invoiceid
-where substr(inv.invoicedate, 0, 5) = '2013'
-group by tra.name
-order by sum(tra.unitprice) desc
-limit 1;
-
---number of units sold
-/*
-select tra.name	, count(inv.invoiceid) as number_of_tracks_sold
-from Track tra 
-join InvoiceLine inl on tra.trackid = inl.trackid
-join Invoice inv on inl.invoiceid = inv.invoiceid
-where substr(inv.invoicedate, 0, 5) = '2013'
-group by tra.name
-having count(inv.invoiceid) > (select count(*) from Track where name = 'Looking For Love')   --this still sucks
-order by count(inv.invoiceid) desc;
-*/
----------------------------------------------------------------------------
 --This is so freakin ugly, BUT IT WORKS!!
 select tra.name	, count(inv.invoiceid) as number_of_tracks_sold
 from Track tra 
@@ -49,3 +18,25 @@ having count(inv.invoiceid) =
 	limit 1
 	)   
 order by count(inv.invoiceid) desc;
+-----------------------------------------------------------
+--number of units sold
+/*
+select tra.name	, count(inv.invoiceid) as number_of_tracks_sold
+from Track tra 
+join InvoiceLine inl on tra.trackid = inl.trackid
+join Invoice inv on inl.invoiceid = inv.invoiceid
+where substr(inv.invoicedate, 0, 5) = '2013'
+group by tra.name
+having count(inv.invoiceid) > (select count(*) from Track where name = 'Looking For Love')   --this still sucks
+order by count(inv.invoiceid) desc;
+*/
+
+--price if you need it
+/*
+select  tra.name, inv.invoicedate, "$" || printf("%.2f", sum(tra.unitprice)) as total_price
+from Track tra join InvoiceLine inl on tra.trackid = inl.trackid
+	join Invoice inv on inl.invoiceid = inv.invoiceid
+where substr(inv.invoicedate, 0, 5) = '2013'
+group by tra.name
+order by sum(tra.unitprice) desc;
+*/
