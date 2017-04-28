fc4a6cf4479b345d8a15a3c6d94cfbe2e0c80435
Talbot <talbotlawrence75@gmail.com>
2017-04-28 11:45:29 -0500

    trying

--- top_2013_track.sql
+++ top_2013_track.sql
@@ -1,5 +1,5 @@
 --Provide a query that shows the most purchased track of 2013.
---should I be using unit price?
+--
 /*
 select  tra.name, inv.invoicedate, "$" || printf("%.2f", sum(inv.[total])) as total_price
 from Track tra join InvoiceLine inl on tra.trackid = inl.trackid
@@ -9,6 +9,7 @@ group by tra.name
 order by sum(inv.[total]) desc
 limit 1;
 */
+--price
 select  tra.name, inv.invoicedate, "$" || printf("%.2f", sum(tra.unitprice)) as total_price
 from Track tra join InvoiceLine inl on tra.trackid = inl.trackid
 	join Invoice inv on inl.invoiceid = inv.invoiceid
@@ -16,3 +17,14 @@ where substr(inv.invoicedate, 0, 5) = '2013'
 group by tra.name
 order by sum(tra.unitprice) desc
 limit 1;
+
+--number of units sold
+select tra.name	, count(inv.invoiceid) as number_of_tracks_sold
+from Track tra 
+join InvoiceLine inl on tra.trackid = inl.trackid
+join Invoice inv on inl.invoiceid = inv.invoiceid
+where substr(inv.invoicedate, 0, 5) = '2013'
+group by tra.name
+having count(inv.invoiceid) > (select count(*) from Track where name = 'Looking For Love')   --this still sucks
+order by count(inv.invoiceid) desc;
+---------------------------------------------------------------------------
